0fa8c4487ccac7c2276e90c93d9f3858403ae045
momenur <momenurislam6@gmail.com>
2023-02-02 12:09:05 +0600

    Added Variable

---
+++ variable.js
@@ -0,0 +1,19 @@
+var number = '33'
+var newIntNumebr = parseInt(number);
+console.log(newIntNumebr);
+console.log(typeof(newIntNumebr));
+
+
+
+
+var foaltNumber = '33.33' ;
+var newNumber = parseInt(number);
+var newFoatNumeber = parseFloat(foaltNumber);
+console.log(newFoatNumeber);
+console.log(typeof(newFoatNumeber));
+
+
+
+var sum = (0.1 + 0.2);
+var result = sum.toFixed(2);
+console.log(result);
